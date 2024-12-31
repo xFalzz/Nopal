b56654e854af0c0bfb8ca3d1b703cab52313dc40
Nawfal <134062420+xFalzz@users.noreply.github.com>
2024-12-31 14:53:49 +0700

    Update ScrollMouseIcon.tsx

--- app/_components/ScrollMouseIcon.tsx
+++ app/_components/ScrollMouseIcon.tsx
@@ -43,7 +43,7 @@ const ScrollMouseIcon: React.FC = () => {
   return isVisible ? (
     <div 
       onClick={scrollToNextSection}
-      className="fixed bottom-20 left-1/2 transform -translate-x-1/2 cursor-pointer hover:opacity-70 transition-opacity duration-300 z-50"
+      className="fixed bottom-12 left-1/2 transform -translate-x-1/2 cursor-pointer hover:opacity-70 transition-opacity duration-300 z-50"
     >
       <div className="w-6 h-10 border-2 border-gray-600 dark:border-gray-300 rounded-full relative">
         <div className="absolute left-1/2 top-2 transform -translate-x-1/2 w-1 h-2 bg-gray-600 dark:bg-gray-300 rounded-full animate-bounce"></div>
